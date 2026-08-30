ee820da7f07577c5d27e83eb4d97ead716dbf983
JPrinstein <228819842+JPrinstein@users.noreply.github.com>
2026-08-30 14:57:30 -0400

    RAG retriever

---
+++ rag/retriever.py
@@ -0,0 +1,18 @@
+from rag.embeddings import embed_texts
+from rag.vector_store import search_index
+
+def retrieve(question, chunks, index, k=2):
+    question_embedding = embed_texts([question])
+
+    distances, indices = search_index(index, question_embedding, k)
+
+    results = []
+
+    for i, index_number in enumerate(indices[0]):
+        results.append({
+            "text": chunks[index_number]["text"],
+            "source": chunks[index_number]["sources"],
+            "distance": distances[0][i]
+        })
+
+    return results
