c5dad97a805c3c9a50c50f170ee591e3b3465132
JPrinstein <228819842+JPrinstein@users.noreply.github.com>
2026-08-30 16:07:58 -0400

    Adjusted prompt and generator settings to shorten responses and help it not ramble on

--- llm/generator.py
+++ llm/generator.py
@@ -1,9 +1,12 @@
 from transformers import pipeline
+from transformers.utils import logging
 import torch
 
 MODEL_NAME = "Qwen/Qwen3-1.7B"
 
 generator = None
+
+logging.set_verbosity_error()
 
 def get_generator():
     global generator
@@ -22,8 +25,11 @@ def generate_answer(question,context): #Will add more context/tools later on
 
         Answer the question using only the provided context. 
 
-        If the context does not contain enough information,
-        say that you do not have enough information.
+        Be concise. Answer in 2-4 sentences.
+
+        If the context does not contain enough information to answer the
+        question confidently, say that the context does not provide enough
+        information.
 
         CONTEXT:
         {context}
@@ -36,9 +42,9 @@ def generate_answer(question,context): #Will add more context/tools later on
 
     output = generator(
         prompt, 
-        max_new_tokens=200,
+        max_new_tokens=120,
         do_sample=True, #False = Picks single most likely token instead of our normal sampling
-        temperature=.7,
+        temperature=.6,
         top_p=.8,
         top_k=20,
         return_full_text=False
