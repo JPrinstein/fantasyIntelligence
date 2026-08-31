3e21e642b039d10691f5b94b865fa94039a8bf16
JPrinstein <228819842+JPrinstein@users.noreply.github.com>
2026-08-30 20:37:52 -0400

    Added a max_distance for the retrieved documents

--- rag/retriever.py
+++ rag/retriever.py
@@ -1,18 +1,23 @@
 from rag.embeddings import embed_texts
 from rag.vector_store import search_index
 
-def retrieve(question, chunks, index, k=2):
+def retrieve(question, chunks, index, k=2, max_distance=1.5):
     question_embedding = embed_texts([question])
 
     distances, indices = search_index(index, question_embedding, k)
 
+    print(distances)
+    print(distances[0])
+
     results = []
 
     for i, index_number in enumerate(indices[0]):
-        results.append({
-            "text": chunks[index_number]["text"],
-            "source": chunks[index_number]["source"],
-            "distance": distances[0][i]
-        })
+        
+        if distances[0][i] <= max_distance:
+            results.append({
+                "text": chunks[index_number]["text"],
+                "source": chunks[index_number]["source"],
+                "distance": distances[0][i]
+            })
 
     return results
