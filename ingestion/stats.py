6ae1a3a2adaec67e80fc92909e088ee6d1dab312
JPrinstein <228819842+JPrinstein@users.noreply.github.com>
2026-09-06 20:30:39 -0400

    Added basic stats loading from nflreadpy

---
+++ ingestion/stats.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+import nflreadpy as nfl
+import polars
+
+RAW_DATA_DIR = Path("data/raw/player_stats")
+
+def load_cached_player_stats(season):
+    path = RAW_DATA_DIR / f"player_stats_{season}.parquet"
+
+    return polars.read_parquet(path)
+
+def load_player_stats(season):
+    stats = nfl.load_player_stats(seasons=[season]) #nflreadypy expects a list as an input
+
+    return stats
+
+def save_player_stats(stats, season):
+    RAW_DATA_DIR.mkdir(
+        parents=True,
+        exist_ok=True
+    )
+
+    path = RAW_DATA_DIR / f"player_stats_{season}.parquet"
+
+    stats.write_parquet(path)
+
+    return path
+
+def get_player_rows(stats, player_name): #Gets player data on a week to week basis
+    return stats.filter(
+        polars.col("player_display_name").str.to_lowercase() == player_name.lower()
+    )
+
+def get_player_stats(player_name, season, week=None): #Can either get the full season stats or a specific week
+    stats = load_cached_player_stats(season)
+
+    player_rows = get_player_rows(
+        stats, player_name
+    )
+
+    if week is not None:
+        player_rows = player_rows.filter(polars.col("week") == week)
+
+    return player_rows
+
+if __name__ == "__main__":
+    season = 2025
+
+    stats = load_cached_player_stats(season)
+
+    player_stats = get_player_rows(
+        stats,
+        "Lamar Jackson"
+    )
+
+    print(player_stats)
